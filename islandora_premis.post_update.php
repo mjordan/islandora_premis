c6bf6393a9fde13251c5337099c5a8c89bf25cb8
Alexander O'Neill <aoneill@upei.ca>
2023-01-24 16:29:54 -0400

    Fix post_update.php file name.

--- islandora_premis.post_update.php
+++ islandora_premis.post_update.php
@@ -1,7 +1,12 @@
 <?php
 
 /**
- * Update routing table after route change.
+ * @file
+ * Post update hooks.
+ */
+
+/**
+ * Implements hook_post_update_NAME().
  */
 function islandora_premis_post_update_permissions_fix_clear_route() {
   \Drupal::service("router.builder")->rebuild();

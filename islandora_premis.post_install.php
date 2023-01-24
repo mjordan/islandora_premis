03ba4a0ccaf36b36ba1a9a509b7c19dd42473da4
Alexander O'Neill <aoneill@upei.ca>
2023-01-24 14:56:04 -0400

    Move install hook to post install function.

--- islandora_premis.post_install.php
+++ islandora_premis.post_install.php
@@ -3,6 +3,6 @@
 /**
  * Update routing table after route change.
  */
-function islandora_premis_update_8001() {
+function islandora_premis_post_update_permissions_fix_clear_route() {
   \Drupal::service("router.builder")->rebuild();
 }
